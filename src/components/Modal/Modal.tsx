144b18600e987ec6ce4b179be07edf0799c7ffde
Ivan Prtain <ivan.prtain@borealis.biz>
2023-07-04 01:47:50 +0200

    Add modal, implement adding new recipe

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import "./Modal.css"
+
+type ModalProps = {
+    children: React.ReactNode
+    isOpen: boolean
+    onClose: () => void
+}
+
+const Modal = ({ children, isOpen, onClose }: ModalProps) => {
+
+
+    if (!isOpen) {
+        return null
+    }
+
+    return (
+        <div className='modal-background'>
+            <div className='modal-container'>
+                <div>
+                    <button onClick={onClose}>X</button>
+                </div>
+                {children}
+            </div>
+        </div>
+    )
+}
+
+export default Modal
